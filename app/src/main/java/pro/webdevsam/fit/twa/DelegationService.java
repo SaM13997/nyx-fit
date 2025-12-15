54856ca1513d02f6f42dd022f194132dfbe8da30
Sarthak Malhotra <sarthakmalhotra251@gmail.com>
2025-12-15 16:47:29 -0500

    bubblewrap integration

---
+++ app/src/main/java/pro/webdevsam/fit/twa/DelegationService.java
@@ -0,0 +1,18 @@
+package pro.webdevsam.fit.twa;
+
+
+import com.google.androidbrowserhelper.playbilling.digitalgoods.DigitalGoodsRequestHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new DigitalGoodsRequestHandler(getApplicationContext()));
+        
+    }
+}
+
